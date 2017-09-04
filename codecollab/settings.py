af262f77d6a69f0ee329e754be2bf9c3bcf304ad
Parv Jain <parv.wutizup@gmail.com>
2017-09-04 21:32:00 +0530

    Added Login views

--- codecollab/settings.py
+++ codecollab/settings.py
@@ -115,7 +115,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+APPEND_SLASH = False
+
+LOGIN_REDIRECT_URL = "/"
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
